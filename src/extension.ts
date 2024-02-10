cb79230f8f6aab71f5f3c97d597b3a9c1250cf7a
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2024-02-10 11:18:41 +0200

    enhanced the ability to pass through fzf arguments

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-const TERMINAL_NAME = 'grep terminal';
+const TERMINAL_NAME = 'g';
 let customTerminal: vscode.Terminal | null = null;
 
 function findTerminal(name: string) {
@@ -56,6 +56,21 @@ function getFzfDirPath() {
   return fzfDirPath ? `${fzfDirPath}/` : '';
 }
 
+function extractFzfArguments(query: string) {
+  let regex = /\|\s*fzf/; // | fzf with any kind of white spaces
+  let match = query.match(regex);
+  if (match) {
+    let lastInd = query.indexOf("|", (match.index || 0) + 1);
+    if (lastInd === -1) {
+      lastInd = query.length;
+    }
+    const fzfOptions = query.substring(( match.index || 0 ) + match.toString().length, lastInd);
+    let rgOptions = query.substring(0, match.index) + query.substring(lastInd);
+    return [rgOptions, fzfOptions];
+  }
+  return [query, ""];
+}
+
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand('vscode-grep.runGrep', async () => {
@@ -68,19 +83,21 @@ export function activate(context: vscode.ExtensionContext) {
       if (query === undefined || query === "") {
         return;
       }
+      const [rgOptions, fzfOptions] = extractFzfArguments(query);
+      
       context.globalState.update("grep.query", query);
 
       const { cwd, type } = getCwd();
-      let targetFile = '';
+      //let targetFile = '';
       let term: vscode.Terminal | null = null;
       if (type === 'file' && vscode.window.activeTextEditor) {
-        targetFile = vscode.window.activeTextEditor.document.uri.fsPath;
+        //targetFile = vscode.window.activeTextEditor.document.uri.fsPath;
         term = showTerminal({ name: query });
       } else {
         term = showTerminal({cwd: cwd, name: query});
       }
 
-      const command = `#!/bin/bash\n${getRgDirPath()}rg --line-number --color=always --colors "path:none" --colors "line:none" --colors "match:fg:0xAA,0xDA,0xFA" ${query} ${targetFile} | ${getFzfDirPath()}fzf -i -e --ansi --color=hl:#5FA392 --bind "ctrl-m:execute-silent(echo {} | cut -f -2 -d ':' | xargs code --goto)" --bind "enter:execute-silent(echo {} | cut -f -2 -d ':' | xargs code --goto)"`;
+      const command = `#!/bin/bash\n${getRgDirPath()}rg --line-number --color=always --colors "path:none" --colors "line:none" --colors "match:fg:0xAA,0xDA,0xFA" ${rgOptions} | ${getFzfDirPath()}fzf -i -e --ansi --color=hl:#5FA392 ${fzfOptions} --bind "ctrl-m:execute-silent(echo {} | cut -f -2 -d ':' | xargs code --goto)" --bind "enter:execute-silent(echo {} | cut -f -2 -d ':' | xargs code --goto)"`;
       const scriptPath = path.join(__dirname, '..', 'src', 'grep.sh');
       fs.writeFile(scriptPath, command, "utf-8", function (err) {
         if (err) {
