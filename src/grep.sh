4c00f63b6023a7fa43820a9708d55edbc645071a
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2024-02-10 19:40:10 +0200

    updated interactive script

--- src/grep.sh
+++ src/grep.sh
@@ -6,17 +6,17 @@ RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
 
 INITIAL_QUERY="${*:-}"
 
-: | fzf -i -e --ansi --disabled --query "$INITIAL_QUERY" \
-    --height 100% \
-    --bind "start:reload($RG_PREFIX {q})+unbind(ctrl-r)" \
+# Switch between Ripgrep mode and fzf filtering mode (CTRL-T)
+: | fzf --ansi --disabled --query "$INITIAL_QUERY" \
+    --bind "start:reload:$RG_PREFIX {q}" \
     --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
-    --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(2. fzf> )+enable-search+rebind(ctrl-r)+transform-query(echo {q} > /tmp/rg-fzf-r; cat /tmp/rg-fzf-f)" \
-    --bind "ctrl-r:unbind(ctrl-r)+change-prompt(1. ripgrep> )+disable-search+reload($RG_PREFIX {q} || true)+rebind(change,ctrl-f)+transform-query(echo {q} > /tmp/rg-fzf-f; cat /tmp/rg-fzf-r)" \
-    --color "hl:-1:underline,hl+:-1:underline:reverse" \
+    --bind 'ctrl-t:transform:[[ ! $FZF_PROMPT =~ ripgrep ]] &&
+      echo "rebind(change)+change-prompt(1. ripgrep> )+disable-search+transform-query:echo \{q} > /tmp/rg-fzf-f; cat /tmp/rg-fzf-r" ||
+      echo "unbind(change)+change-prompt(2. fzf> )+enable-search+transform-query:echo \{q} > /tmp/rg-fzf-r; cat /tmp/rg-fzf-f"' \
+    --color "hl:#5FA392:underline,hl+:#5FA392:underline:reverse" \
     --prompt '1. ripgrep> ' \
     --delimiter : \
-    --header '╱ CTRL-R (ripgrep mode) ╱ CTRL-F (fzf mode) ╱' \
+    --header 'CTRL-T: Switch between ripgrep/fzf' \
     --preview 'bat --color=always {1} --highlight-line {2}' \
-    --preview-window 'up,40%,border-bottom,+{2}+3/3,~3' \
+    --preview-window 'up,50%,border-bottom,+{2}+3/3,~3' \
     --bind 'enter:execute-silent(echo "{1}:{2}:{3}" | xargs code --goto)'
-
